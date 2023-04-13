05f6f16ae9b3ed5868acededda387d0911a9cf48
UmamaheshMaxwell <maheshmeka16@gmail.com>
2023-04-13 23:14:08 +0530

    added node service

--- cloud-run/main.tf
+++ cloud-run/main.tf
@@ -1,6 +1,14 @@
+/*
+    * This is needed for cloud run 
+*/
 locals {
   service_account = "terraform@gcp-training-377619.iam.gserviceaccount.com"
+  instances = [google_cloud_run_service.tf_cloud_run1, google_cloud_run_service.tf_cloud_run2]
 }
+
+/*
+    * This is needed to enable "Allow Unauthenticated"
+*/
 
 data "google_iam_policy" "tf_no_auth" {
   binding {
@@ -12,13 +20,35 @@ data "google_iam_policy" "tf_no_auth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "tf_no_auth" {
-    location = google_cloud_run_service.tf_cloud_run.location
-    project = google_cloud_run_service.tf_cloud_run.project
-    service = google_cloud_run_service.tf_cloud_run.name
-    policy_data = data.google_iam_policy.tf_no_auth.policy_data
+  for_each = {
+    for key, value in local.instances : key => value
+  }
+  service  = each.value.name
+  location = each.value.location
+  project  = each.value.project
+  policy_data = data.google_iam_policy.tf_no_auth.policy_data
 }
 
-resource "google_cloud_run_service" "tf_cloud_run" {
+resource "google_cloud_run_service" "tf_cloud_run1" {
+  name     = "node-app-service"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        // Make sure your image is avilabe with in google container registry
+        image = "gcr.io/gcp-training-377619/node-app:v1"
+      }
+      service_account_name = local.service_account
+    }
+  }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_service" "tf_cloud_run2" {
   name     = "cloud-run-service"
   location = "us-central1"
 
